a579e328e0410f1c34167d180574122421b2ed29
Vismaya <vismayas1812@gmail.com>
2024-05-18 07:33:03 +0530

    added with sub and div

--- src/calculator.py
+++ src/calculator.py
@@ -8,7 +8,13 @@ def multiply(a, b):
     return a * b
 
 def sub(a, b):
-    return a - b   
+    return a - b  
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero error" 
 
 
 
